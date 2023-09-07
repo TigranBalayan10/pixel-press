06e86949fa88da9e733425fa382e1a5fded8a4a8
TigranBalayan10 <tigranbalayan10@gmail.com>
2023-09-07 01:26:24 -0700

    add tooltip and swith for file upload input

--- app/order/business-card/page.tsx
+++ app/order/business-card/page.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+"use client"
+
+
+import React, { useState } from 'react'
 import {
     Select,
     SelectContent,
@@ -7,7 +10,17 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
+import { Separator } from '@/components/ui/separator'
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Switch } from '@/components/ui/switch'
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
 import ProductList from "@/lib/Data/ProductList.json"
 import { productList } from "@/lib/types"
 import Image from 'next/image'
@@ -31,22 +44,22 @@ const getBusinessCardOptions = <T extends OptionType>(type: T): ValueType<T>[] =
 type RenderSelectProps = {
     options: (string | number)[];
     placeholder: string;
-  };
-  
-  const RenderSelect: React.FC<RenderSelectProps> = ({ options, placeholder }) => (
+};
+
+const RenderSelect: React.FC<RenderSelectProps> = ({ options, placeholder }) => (
     <Select>
-      <SelectTrigger className="w-[300px] md:w-[400px]">
-        <SelectValue placeholder={placeholder} />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          {options.map((option, id) => (
-            <SelectItem value={String(option)} key={id}>{String(option)}</SelectItem>
-          ))}
-        </SelectGroup>
-      </SelectContent>
+        <SelectTrigger className="w-[300px] md:w-[400px]">
+            <SelectValue placeholder={placeholder} />
+        </SelectTrigger>
+        <SelectContent>
+            <SelectGroup>
+                {options.map((option, id) => (
+                    <SelectItem value={String(option)} key={id}>{String(option)}</SelectItem>
+                ))}
+            </SelectGroup>
+        </SelectContent>
     </Select>
-  );
+);
 
 
 
@@ -55,8 +68,11 @@ type RenderSelectProps = {
 
 
 const OrderBusinessCard = () => {
+
+    const [isSwitchChecked, setIsSwitchChecked] = useState<boolean>(false);
+
     return (
-        <form className='flex justify-center'>
+        <form className='flex justify-center' onSubmit={(e) => e.preventDefault()}>
             <div className="card w-96 m-4 md:w-1/2 bg-secondary text-primary-content mt-5">
 
                 <div className="card-body gap-4">
@@ -65,6 +81,9 @@ const OrderBusinessCard = () => {
                         <figure>
                             <Image src="/business-card_mockup.jpg" className="rounded-lg drop-shadow-md" alt="Color choice stock photo" width={350} height={700} />
                         </figure>
+                        <div>
+                            <Separator orientation='vertical' />
+                        </div>
                         <div className='flex flex-col justify-start gap-y-3'>
                             {[
                                 { type: 'sizes', placeholder: 'Select a size' },
@@ -76,6 +95,29 @@ const OrderBusinessCard = () => {
                             ].map(({ type, placeholder }) => (
                                 <RenderSelect options={getBusinessCardOptions(type as OptionType)} placeholder={placeholder} key={type} />
                             ))}
+                            <div className="grid w-full max-w-sm items-center gap-1.5">
+
+                                <div className="flex justify-between gap-x-4">
+                                    <Label htmlFor="design_back">Upload Front</Label>
+                                </div>
+                                <Input id="front" type="file" />
+                            </div>
+                            <div className="grid w-full max-w-sm items-center gap-1.5">
+                                <div className="flex justify-between gap-x-4">
+                                    <Label htmlFor="design_back">Upload Back</Label>
+                                    <TooltipProvider>
+                                        <Tooltip>
+                                            <TooltipTrigger>
+                                                <Switch checked={isSwitchChecked} onCheckedChange={checked => setIsSwitchChecked(checked)} />
+                                            </TooltipTrigger>
+                                            <TooltipContent>No Back</TooltipContent>
+                                        </Tooltip>
+                                    </TooltipProvider>
+                                </div>
+                                {
+                                    isSwitchChecked ? <Input id="back" disabled placeholder='No Back' /> : <Input id="back" type="file" />
+                                }
+                            </div>
                         </div>
                     </div>
                     <div className="card-actions justify-end">
